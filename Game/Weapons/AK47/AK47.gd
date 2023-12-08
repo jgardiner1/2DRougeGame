6a51b7a6bdc98c7fad37dfa00fb0a364fdf23e2b
jgardiner1 <jamesgardiner1@live.co.uk>
2023-12-08 23:53:10 +0000

    Player can use 2 hands to pick up 2 1-handed weapons or 1 2-handed weapon. Implemented basic throw for 2 handed weapon

--- Game/Weapons/AK47/AK47.gd
+++ Game/Weapons/AK47/AK47.gd
@@ -1,12 +1,15 @@
 extends RigidBody2D
 
 var can_pickup = true
+var two_handed = true
+
+var linear_v
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
-
+	
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
