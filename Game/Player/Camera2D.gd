474d174ebbee0781c231a3ce7abd204502f89bfc
jgardiner1 <jamesgardiner1@live.co.uk>
2023-12-10 17:33:26 +0000

    removed method in camera script

--- Game/Player/Camera2D.gd
+++ Game/Player/Camera2D.gd
@@ -1,7 +1,7 @@
 extends Camera2D
 
-const smooth_lean := 10.0
-const scale_lean := 0.2
+@export var smooth_lean := 10.0
+@export var scale_lean := 0.2
 
 func lean_camera_towards_mouse_(delta:float) -> void:
 	var mouse_position := get_global_mouse_position()
@@ -11,14 +11,5 @@ func lean_camera_towards_mouse_(delta:float) -> void:
 	var lean := direction_to_mouse * distance_to_mouse * scale_lean
 	offset = lerp(offset, lean, delta * smooth_lean)
 
-
-func match_player_position_() -> void:
-	if get_parent().has_node("Player"):
-		position = get_node("Player").position
-	else:
-		print("missing")
-
 func _process(delta) -> void:
 	lean_camera_towards_mouse_(delta)
-	match_player_position_()
-
